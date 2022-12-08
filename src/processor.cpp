a106909d79952f6e195357d81decb8cc9b7a75c0
maurosterpin <msterpindev@gmail.com>
2022-12-08 10:43:15 +0000

    processor

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,7 +1,16 @@
 #include "processor.h"
 #include "linux_parser.h"
+#include <string>
+#include <vector>
 
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() { 
-    return LinuxParser::Jiffies(); 
+    std::vector<long> cpu_utilization{};
+    for (auto pid : LinuxParser::CpuUtilization()) {
+        cpu_utilization.emplace_back(std::stol(pid));
+    }
+    float total_jiffies, idle_jiffies;
+    total_jiffies = cpu_utilization[LinuxParser::kUser_] + cpu_utilization[LinuxParser::kNice_] + cpu_utilization[LinuxParser::kSystem_] + cpu_utilization[LinuxParser::kIdle_] + cpu_utilization[LinuxParser::kIOwait_] + cpu_utilization[LinuxParser::kIRQ_] + cpu_utilization[LinuxParser::kSoftIRQ_] + cpu_utilization[LinuxParser::kSteal_];
+    idle_jiffies = cpu_utilization[LinuxParser::kIdle_] + cpu_utilization[LinuxParser::kIOwait_];
+    return (total_jiffies - idle_jiffies) / total_jiffies;
     }
